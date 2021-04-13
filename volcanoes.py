26d5baa815343ef64e14a852b24aa764c130163a
Mikroi <79386138+Mikroi@users.noreply.github.com>
2021-04-13 19:47:31 -0400

    Additional basic features for volcanoe map.

--- volcanoes.py
+++ volcanoes.py
@@ -5,12 +5,24 @@ data = pandas.read_csv("Webmap_datasources/Volcanoes.txt")
 
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif elevation <= 2000:
+        return 'orange'
+    elif elevation <= 3000:
+        return 'blue'
+    else:
+        return 'red'
+
 map = folium.Map(location=[34.7, -94.06], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi! I'm a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 14, popup=str(el)+" m", fill_color=color_producer(el), color = 'white', fill_opacity = 0.6))
 
 map.add_child(fg)
 

4b005498011dc3aeae3773b16c57536341e374a6
Mikroi <79386138+Mikroi@users.noreply.github.com>
2021-04-17 14:26:20 -0400

    Basics of Choropleth Map

--- worldmap.py
+++ worldmap.py
@@ -19,14 +19,19 @@ def color_producer(elevation):
 
 map = folium.Map(location=[34.7, -94.06], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 14, popup=str(el)+" m", fill_color=color_producer(el), color = 'white', fill_opacity = 0.6))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 14, popup=str(el)+" m", fill_color=color_producer(el), color = 'white', fill_opacity = 0.6))
 
+fgp = folium.FeatureGroup(name="Population")
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
-style_function=lambda x: {'fillColor': 'yellow'}))
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
 map.save("Map3.html")
